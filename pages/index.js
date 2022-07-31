85efb19d1e048aaa3a6e200cd7f075f1c3ebd21a
ys <lhyahoo0119@gmail.com>
2022-07-31 20:57:08 +0900

    [Update] router URL masking

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,16 @@ import Helmet from '../components/Helmet';
 
 export default function Home({ results }) {
   const router = useRouter();
-  const onClick = (id) => {
-    router.push(`movies/${id}`);
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
   };
   return (
     <div>
@@ -16,12 +24,20 @@ export default function Home({ results }) {
           <div
             key={movie.id}
             onClick={() => {
-              onClick(movie.id);
+              onClick(movie.id, movie.original_title);
             }}
           >
-            <Link href={`movies/${movie.id}`}>
+            <Link
+              href={{
+                pathname: `movies/${movie.id}`,
+                query: {
+                  title: movie.original_title,
+                },
+              }}
+              as={`movies/${movie.id}`}
+            >
               <a>
-                <h4>{movie.original_title}</h4>
+                <div>{movie.original_title}</div>
               </a>
             </Link>
             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
