0be4b7bbd35a274c4154cc8736c6c2a3d88f9007
ys <lhyahoo0119@gmail.com>
2022-07-31 17:41:02 +0900

    [Add] dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import Helmet from '../components/Helmet';
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id) => {
+    router.push(`movies/${id}`);
+  };
   return (
     <div>
       <Helmet title='Home' />
       {results?.map((movie) => {
         return (
-          <div key={movie.id}>
-            <h4>{movie.original_title}</h4>
+          <div
+            key={movie.id}
+            onClick={() => {
+              onClick(movie.id);
+            }}
+          >
+            <Link href={`movies/${movie.id}`}>
+              <a>
+                <h4>{movie.original_title}</h4>
+              </a>
+            </Link>
             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           </div>
         );
