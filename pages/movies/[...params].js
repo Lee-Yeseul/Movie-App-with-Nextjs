a3f0ed705d31b518408ee7b5e26f2f60a9a5de33
ys <lhyahoo0119@gmail.com>
2022-08-12 18:40:23 +0900

    [Update] movie detail page 정보 받아오기

--- pages/movies/[...params].js
+++ pages/movies/[...params].js
@@ -1,22 +1,30 @@
-import { useRouter } from 'next/router';
+import axios from 'axios';
 
-export default function Detail({ params, results }) {
-  const router = useRouter();
-  // console.log(router);
+export default function Detail({ params, data }) {
   const [title, id] = params;
+  const { belongs_to_collection, original_language, genres } = data;
+  console.log(genres);
+
   return (
     <div>
       <h4>{title || 'Loading...'}</h4>
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${belongs_to_collection.poster_path}`}
+      />
+      <h4>{original_language}</h4>
+      {genres?.map((genre, idx) => {
+        return <div key={idx}>{genre.name}</div>;
+      })}
     </div>
   );
 }
 
-export function getServerSideProps({ params: { params } }) {
-  const { results } = await (
-    await axios.get(`http://localhost:3000/api/movies/${params.id}`)
-  ).data;
+export async function getServerSideProps({ params: { params } }) {
+  const [title, id] = params;
+  const res = await axios.get(`http://localhost:3000/api/movies/${id}`);
+  const data = res.data;
 
   return {
-    props: { params, results },
+    props: { params, data },
   };
 }
