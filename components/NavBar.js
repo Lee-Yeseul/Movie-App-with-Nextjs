8b796820dea3390bba9673ff15f95923dc0c4022
ys <lhyahoo0119@gmail.com>
2022-08-22 00:34:57 +0900

    [style] navBar style 변경

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,28 +6,50 @@ export default function NavBar() {
   return (
     <nav>
       <img src='/vercel.svg' />
-      <div>
-        <Link href='/'>
-          <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
-        </Link>
-        <Link href='/about'>
-          <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
-        </Link>
-      </div>
+      <ul>
+        <li>
+          <Link href='/'>
+            <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/about'>
+            <a className={router.pathname === '/about' ? 'active' : ''}>
+              About
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/koreanMovies'>
+            <a className={router.pathname === '/koreanMovies' ? 'active' : ''}>
+              Korean Movies
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/globalMovies'>
+            <a className={router.pathname === '/globalMovies' ? 'active' : ''}>
+              Global Movies
+            </a>
+          </Link>
+        </li>
+      </ul>
       <style jsx>{`
         nav {
           display: flex;
-          gap: 10px;
-          flex-direction: column;
-          align-items: center;
-          padding-top: 20px;
+          justify-content: space-between;
+          padding-top: 1rem;
           padding-bottom: 10px;
-          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
-            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+          // box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+          //   rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
         }
         img {
           max-width: 100px;
           margin-bottom: 5px;
+        }
+        ul {
+          display: flex;
+          gap: 1.5rem;
         }
         nav a {
           font-weight: 600;
