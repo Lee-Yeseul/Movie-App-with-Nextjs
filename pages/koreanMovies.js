9ee3803f7e7d57dfc89a5a3d0497b5ffcafe6e12
ys <lhyahoo0119@gmail.com>
2022-08-23 23:44:57 +0900

    [Add] 날짜 계산 함수 추가

--- pages/koreanMovies.js
+++ pages/koreanMovies.js
@@ -3,11 +3,19 @@ import { useEffect } from 'react';
 import Helmet from '../components/Helmet';
 
 export default function KoreanMovies() {
+  const date = new Date();
+
+  const year = date.getFullYear();
+  const month = ('0' + (1 + date.getMonth())).slice(-2);
+  const day = date.getDate() - 1;
+
+  const targetDt = `${year}${month}${day}`;
+
   async function getAPI() {
     const res = await axios.get(
-      `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${process.env.NEXT_PUBLIC_KOR_API_KEY}&targetDt=20220801`
+      `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${process.env.NEXT_PUBLIC_KOR_API_KEY}&targetDt=${targetDt}`
     );
-    console.log(res);
+    console.log(res.data);
   }
   useEffect(() => {
     getAPI();
